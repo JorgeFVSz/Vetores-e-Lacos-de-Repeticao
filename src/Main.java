1d52388eaf4f4740273cb2a3fbed941e0631b426
JorgeFVSz <jorge.silvestre@zup.com.br>
2024-12-05 14:50:32 -0300

    feat(loops): adiciona laço for para imprimir vetor de 5 elementos

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] vetor1 = {10, 24, 32, 40, 65};
+
+        for (int i = 0; i < vetor1.length; i++) {
+            System.out.println(vetor1[i]);
+        }
+    }
+}
